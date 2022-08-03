4ef73a62f01a56648fb6361e8d3b91f414fa45b0
Othon Oliveira <othonoliveira08@gmail.com>
2022-08-02 22:33:24 -0300

    Terminando Requisitos Obrigatórios

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-window.onload = function () {
-  let value = 0;
-  for (let index = 0; index < 16; index += 1) {
-    for (let index2 = 0; index2 < 16; index2 += 1) {
-      let newDiv = document.createElement('div');
-      newDiv.setAttribute('id', `div${(value)}`)
-      let divTable = document.getElementById('color-table');
+window.onload = function creatPixels() {
+  for (let index = 0; index < 5; index += 1) {
+    for (let index2 = 0; index2 < 5; index2 += 1) {
+      const newDiv = document.createElement('div');
+      newDiv.setAttribute('class', 'pixel');
+      const divTable = document.getElementById('pixel-board');
       divTable.appendChild(newDiv);
-      value += 1;
+    }
+  }
+};
+
+function newSelection(event) {
+  const colors = document.querySelector('.selected');
+  colors.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+const pallet = document.querySelector('#color-palette');
+pallet.addEventListener('click', newSelection);
+
+function paint(event) {
+  const selectedColor = document.querySelector('.selected');
+  event.target.classList.remove('undefined');
+  event.target.classList.remove('red');
+  event.target.classList.remove('blue');
+  event.target.classList.remove('coral');
+  event.target.classList.remove('black');
+  event.target.classList.add(selectedColor.classList[1]);
+}
+
+const board = document.querySelector('#pixel-board');
+board.addEventListener('click', paint);
+
+function cleaner() {
+  const boardToClean = document.querySelectorAll('.pixel');
+  for (let index = 0; index < 25; index += 1) {
+    if (boardToClean[index].classList.length > 0) {
+      boardToClean[index].classList.remove('red');
+      boardToClean[index].classList.remove('blue');
+      boardToClean[index].classList.remove('black');
+      boardToClean[index].classList.remove('coral');
     }
   }
 }
 
-function colorSelection(event) {
-  let selectedColor = event.target.idName;
-  let table = document.querySelector('color-table');
-  table.addEventListener('click', function(event) {
-    let reciverColor = document.getElementById()
-    reciverColor= selectedColor;
-  })
-}
-
-let pallet = document.querySelector('color-palette');
-pallet.addEventListener('click', colorSelection);
+const boardCleaner = document.getElementById('clear-board');
+boardCleaner.addEventListener('click', cleaner);
